3b77d9a9ab14b171993e9b4ad4a0bba46b97027f
Chaitra P <chaitra.p@bp.com>
2022-10-12 11:40:50 +0530

    Check Attendence

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage Problem");
+
+            EmployeeCheck.CheckAttendence();
         }
     }
 }
